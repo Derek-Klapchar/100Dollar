6d1945d71ac6aab27fb933ac02dc82fbcc7cee0b
Derek T. Klapchar <derek.klapchar@gmail.com>
2018-05-24 01:04:10 -0400

    This program gives the correct bills for a whole dollar amount

---
+++ dollar2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void){
+
+	int dollar;
+	int amount;
+
+	printf("Enter a whole dollar amount: ");
+	scanf("%d", &amount);
+
+
+	dollar = amount /100;
+	printf("\n100 bills: %d\n", dollar);
+	amount = amount - (dollar * 100);
+
+	dollar = amount /20;
+	printf("\n$20 bills: %d\n", dollar);
+	amount = amount - (dollar * 20);
+
+	dollar = amount /10;
+	printf("\n$10 bills: %d\n", dollar);
+	amount = amount - (dollar * 10);
+
+	dollar = amount /5;
+	printf("\n$5 bills: %d\n", dollar);
+	amount = amount - (dollar * 5);
+	
+	dollar = amount /1;
+	printf("\n$1 bills: %d\n", dollar);
+	amount = amount - (dollar * 1);
+
+	return 0;
+}
